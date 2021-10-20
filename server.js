cf6e1c170107f4ca0221583d0790ffcf19c4289d
Thu Phan <alicebe.phan@gmail.com>
2021-10-19 23:32:30 -0500

    Started writing get route and tested it in insomnia, it worked!

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+const noteData = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 // create app variable and call express() function so that the whole library basically comes in 
 //as a big function so I can execute and put it in the variable
@@ -13,13 +13,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// GET Route for homepage
+// GET Routes for homepage and the next page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+
 
 
 // listening, at which port to listen (this case is 3001)
