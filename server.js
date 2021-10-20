216c3c629001089ef73e15c29b397effacc80526
Thu Phan <alicebe.phan@gmail.com>
2021-10-19 22:14:43 -0500

    Imported all packages, created variables app and port and told it which port to listen at

---
+++ server.js
@@ -0,0 +1,19 @@
+// import all the packages 
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+// create app variable and call express() function so that the whole library basically comes in 
+//as a big function so I can execute and put it in the variable
+const app = express();
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// listening, at which port to listen (this case is 3001)
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
